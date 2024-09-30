6ef2d66b5799bf46e0107acf259d9123a559b5b3
pvega7721 <pvega7721@ceuandalucia.es>
2024-09-30 20:33:30 +0200

    .

---
+++ EjTema2/Fechas/Ej3/js/Ej3.js
@@ -0,0 +1,4 @@
+var dia = prompt("Introduce sólo el día de tu cumpleaños");
+var mes = prompt("Introduce sólo el mes de tu cumpleaños");
+var anyoActual = new Date().getFullYear;
+for (i = anyoActual; (i = x); i++) {}
