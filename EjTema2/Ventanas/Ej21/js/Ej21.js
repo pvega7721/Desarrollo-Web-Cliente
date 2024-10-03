3a726fede4c8f3894366322fa0d2193091bd39e5
Pablo Vega <pablo.veflo.pvf@icloud.com>
2024-10-03 09:31:58 +0200

    .

--- EjTema2/Ventanas/Ej21/js/Ej21.js
+++ EjTema2/Ventanas/Ej21/js/Ej21.js
@@ -2,6 +2,6 @@ var ventana1;
 var ventana2;
 
 function abrirVentanas() {
-  ventana1 = window.open("ventana1.html");
+  ventana1 = window.open("ventana1.html", "ventana 1", "");
   ventana2 = window.open("ventana2.html");
 }
