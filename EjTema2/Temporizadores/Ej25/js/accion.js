72ff30e2e5e2ea2422887cc7aeda06f0545d8f67
Pablo Vega <pvega7721@ceuandalucia.es>
2024-10-13 15:47:55 +0200

    .

--- EjTema2/Temporizadores/Ej25/js/accion.js
+++ EjTema2/Temporizadores/Ej25/js/accion.js
@@ -1,5 +1,5 @@
+//1. abrir ventana
 var ventanaEmergente;
-
 function abrirVentana() {
   ventanaEmergente = window.open(
     "ventanaEmergente.html",
@@ -7,3 +7,8 @@ function abrirVentana() {
     "width=500, height=500"
   );
 }
+//2. crear un temporizador para que la ventana se cierre en los segundos introducidos.
+var seg = parseInt(document.getElementById("segundos").value);
+setTimeout(function () {
+  ventanaEmergente.close();
+}, seg * 1000);
