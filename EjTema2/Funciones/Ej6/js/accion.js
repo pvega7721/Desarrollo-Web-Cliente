9d296387135783aa3f7754239a5e2c03a967fa66
Pablo Vega <pvega7721@ceuandalucia.es>
2024-10-15 11:58:11 +0200

    .

--- EjTema2/Funciones/Ej6/js/accion.js
+++ EjTema2/Funciones/Ej6/js/accion.js
@@ -1,14 +1,18 @@
 var fecha = new Date();
-var ventana = window.open(
-  "../ventanaEmergente.html",
-  "",
-  "width=400, height=400"
-);
+var ventana;
 function abrirVentana() {
+  ventana = window.open(
+    "file:///C:/Github-asignaturas/Desarrollo-Web-Cliente/EjTema2/Funciones/Ej6/ventanaEmergente.html",
+    "ventanaEmergente",
+    "width=400, height=400"
+  );
   if (fecha.getDate() == 0 || fecha.getDate() == 6) {
+    ventana.document.body.innerHTML(
+      "<img src='imagenes/feliz.png' alt='finFeSemana'>"
+    );
   } else {
     ventana.document.body.innerHTML(
-      "<img src='imagenes/feliz.png' alt='findesemana'>"
+      '<img src="imagenes/triste.jpg" alt="noFinDeSemana" />'
     );
   }
 }
