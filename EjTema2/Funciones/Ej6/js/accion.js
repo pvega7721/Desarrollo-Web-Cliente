e4e7e7540e26d6472ad3c5e100c21a77b1ff1393
pvega7721 <pvega7721@ceuandalucia.es>
2024-10-16 15:54:01 +0200

    .

--- EjTema2/Funciones/Ej6/js/accion.js
+++ EjTema2/Funciones/Ej6/js/accion.js
@@ -2,14 +2,15 @@ var fecha = new Date();
 var ventana;
 function abrirVentana() {
   ventana = window.open(
-    "../ventanaEmergente.html",
+    "./ventanaEmergente.html",
     "ventanaEmergente",
     "width=400, height=400"
   );
+  ventana.document.write("a"); //Solo funciona si escribo algo antes¿¿¿¿
   if (fecha.getDay() == 0 || fecha.getDay() == 6) {
     //Es getDay() no getDate(), getDay es la que da el número de la semana
     ventana.document.body.innerHTML +=
-      "<img src='imagenes/feliz.png' alt='finFeSemana'>";
+      "<img src='imagenes/feliz.png' alt='finDeSemana'>";
   } else {
     ventana.document.body.innerHTML +=
       '<img src="imagenes/triste.jpg" alt="noFinDeSemana" />';
--- EjTema2/Funciones/Ej7/js/accion.js
+++ EjTema2/Funciones/Ej7/js/accion.js
@@ -1,18 +1,28 @@
 var ventana;
+
 function listaOrdenada() {
   ventana = window.open(
-    "../ventana.html",
+    "./ventana.html",
     "ventanaEmergente",
     "width=700, height=400"
   );
-  var filas = ventana.prompt("Introduce un número entre 1 y 20"); //No funciona pero lo voy a hacer suponiendo que tiene un número
-  ventana.close();
-  var imprimirFilas = (filas) => {
-    document.body.innerHTML += "<ol>";
-    for (let i = 0; i < filas; i++) {
-      document.body.innerHTML += "<li></li>";
-    }
-    document.body.innerHTML += "</ol>";
-  };
-  imprimirFilas;
+
+  if (ventana) {
+    var filas = ventana.prompt("Introduce un número entre 1 y 20"); //No funciona
+
+    var imprimirFilas = (filas) => {
+      document.body.innerHTML += "<ol>";
+
+      for (let i = 0; i < filas; i++) {
+        document.body.innerHTML += `<li>${i + 1}</li>`;
+      }
+
+      ventana.close();
+      document.body.innerHTML += "</ol>";
+    };
+
+    imprimirFilas(filas);
+  }
 }
+
+listaOrdenada();
