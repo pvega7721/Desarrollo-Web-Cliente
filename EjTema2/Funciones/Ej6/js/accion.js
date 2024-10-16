2eb0407d5ce89af91953c5d44ea67f7842c63891
pvega7721 <pvega7721@ceuandalucia.es>
2024-10-16 16:13:52 +0200

    .

--- EjTema2/Funciones/Ej6/js/accion.js
+++ EjTema2/Funciones/Ej6/js/accion.js
@@ -1,12 +1,13 @@
-var fecha = new Date();
-var ventana;
 function abrirVentana() {
   ventana = window.open(
     "./ventanaEmergente.html",
     "ventanaEmergente",
     "width=400, height=400"
   );
-  ventana.document.write("a"); //Solo funciona si escribo algo antes¿¿¿¿
+}
+
+//ventana.document.write("a"); //Solo funciona si escribo algo antes¿¿¿¿
+/*
   if (fecha.getDay() == 0 || fecha.getDay() == 6) {
     //Es getDay() no getDate(), getDay es la que da el número de la semana
     ventana.document.body.innerHTML +=
@@ -17,3 +18,4 @@ function abrirVentana() {
     //innerHTML no se esribe con (), se escribe con =
   }
 }
+*/
