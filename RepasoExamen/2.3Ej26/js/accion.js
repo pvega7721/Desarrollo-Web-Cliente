fea6f909cb1903edc2f6d413a00c6bc3e541f675
Pablo Vega <pvega7721@ceuandalucia.es>
2024-11-20 12:46:28 +0100

    .

--- RepasoExamen/2.3Ej26/js/accion.js
+++ RepasoExamen/2.3Ej26/js/accion.js
@@ -1,10 +1,27 @@
 var ventana;
+var intervalo;
+var tiempoRestante;
 var abrirVentana = function () {
-    //Abre la ventana
-  ventana = window.open("Emergente.html", "nuevaVentana", "width=400,height=200");
+  //accede al campo cuando le ha dado al botón
+  var tiempoIntroducido = document.getElementById("segundos").value;
+  //Abre la ventana
+  ventana = window.open(
+    "Emergente.html",
+    "nuevaVentana",
+    "width=400,height=200"
+  );
+  //asigna el valor introducido en el input
+  tiempoRestante = tiempoIntroducido;
+  intervalo = setInterval(() => {
+    //Muestra el temporizador en la ventana
+    ventana.document.getElementById("cuentaAtras").textContent = tiempoRestante;
+    if (tiempoRestante <= 0) {
+      clearInterval(intervalo);
+      ventana.close();
+      document.getElementById(
+        "resultado"
+      ).textContent = `Ya han pasado ${tiempoIntroducido} segundos y se ha cerrado la ventana emergente`;
+    }
+    tiempoRestante--;
+  }, 1000);
 };
-
-inicio = () => {
-    
-};
-document.addEventListener("DOMContentLoaded", inicio);
