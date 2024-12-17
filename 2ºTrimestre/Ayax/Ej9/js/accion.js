351a43dd2d3c15070f4be3359781a90c7cf559c5
Pablo Vega <pvega7721@ceuandalucia.es>
2024-12-17 10:30:09 +0100

    .

--- 2ºTrimestre/Ayax/Ej9/js/accion.js
+++ 2ºTrimestre/Ayax/Ej9/js/accion.js
@@ -1,20 +1,41 @@
+var div = document.getElementById("posts-list");
+
 window.addEventListener("DOMContentLoaded", () => {
   var boton = document.getElementById("boton");
   boton.addEventListener("click", (e) => {
     e.preventDefault();
-    cargarPosts();
+    cargarExtras();
   });
+  cargarPosts();
 });
 
 function cargarPosts() {
   fetch("https://jsonplaceholder.typicode.com/posts?_page=1&_limit=5").then(
     (response) =>
       response.json().then((posts) => {
-        var div = document.getElementById("posts-list");
-
         posts.forEach((post) => {
-          div.innerHTML = `${post.title}`;
+          var divPost = document.createElement("div");
+          divPost.innerHTML = `Titulo: ${post.title} <br> Cuerpo: ${post.body} <br><br>`;
+          div.appendChild(divPost);
         });
       })
   );
 }
+
+var i = 2;
+function cargarExtras() {
+  fetch(`https://jsonplaceholder.typicode.com/posts?_page=${i}&_limit=5`).then(
+    (response) =>
+      response.json().then((posts) => {
+        div.innerHTML = "";
+        posts.forEach((post) => {
+          var divPost = document.createElement("div");
+          divPost.innerHTML = `Titulo: ${post.title} <br> Cuerpo: ${post.body} <br><br>`;
+          div.appendChild(divPost);
+        });
+      })
+  );
+  i++;
+}
+
+//cargarComentarios
