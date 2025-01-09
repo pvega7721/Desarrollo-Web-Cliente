b342d0b0a13e38c024197846a5e9e1d5bb8ca4f9
pvega7721 <pvega7721@ceuandalucia.es>
2025-01-09 15:16:00 +0100

    Refactor código de acción.js para mejorar la legibilidad y consistencia

--- 2ºTrimestre/Ayax/Ej2/Fetch/js/accion.js
+++ 2ºTrimestre/Ayax/Ej2/Fetch/js/accion.js
@@ -1,23 +1,21 @@
 window.addEventListener("DOMContentLoaded", () => {
-    var boton = document.getElementById("boton");
-    boton.addEventListener("click", (e) =>{
-        e.preventDefault();
-        mostrarDatos();
-    })
+  var boton = document.getElementById("boton");
+  boton.addEventListener("click", (e) => {
+    e.preventDefault();
+    mostrarDatos();
+  });
 });
 
-function mostrarDatos(){
-    fetch("https://jsonplaceholder.typicode.com/users").then((response) => 
+function mostrarDatos() {
+  fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
     response.json().then((usuarios) => {
-        console.log(usuarios);
-        //Estructura de la tabla
-        tabla = document.createElement("table");
-        cabecera = document.createElement("thead");
-        tr = document.createElement("tr");
-        document.createElement("th");
-        body = document.createElement("tbody");
-
+      console.log(usuarios);
+      //Estructura de la tabla
+      tabla = document.createElement("table");
+      cabecera = document.createElement("thead");
+      tr = document.createElement("tr");
+      document.createElement("th");
+      body = document.createElement("tbody");
     })
-    
-    )
+  );
 }
