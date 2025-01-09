8a8eb628d3938cd8835531310a754122443e556d
Pablo Vega <pvega7721@ceuandalucia.es>
2025-01-09 11:48:29 +0100

    .

---
+++ 2ºTrimestre/Ayax/Ej2/Fetch/js/accion.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", () => {
+    var boton = document.getElementById("boton");
+    boton.addEventListener("click", (e) =>{
+        e.preventDefault();
+        mostrarDatos();
+    })
+});
+
+function mostrarDatos(){
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) => 
+    response.json().then((usuarios) => {
+        console.log(usuarios);
+        //Estructura de la tabla
+        tabla = document.createElement("table");
+        cabecera = document.createElement("thead");
+        tr = document.createElement("tr");
+        document.createElement("th");
+        body = document.createElement("tbody");
+
+    })
+    
+    )
+}
