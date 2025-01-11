9933ffd62bb29fb4c40d98d2765570f16366e6b9
Pablo Vega <pvega7721@ceuandalucia.es>
2025-01-11 13:39:33 +0100

    Actualizar Ejercicio 2 para utilizar Fetch y XMLHttpRequest, mejorando la carga y visualización de datos en tablas

--- 2ºTrimestre/Ayax/Ej2/Fetch/js/accion.js
+++ 2ºTrimestre/Ayax/Ej2/Fetch/js/accion.js
@@ -1,21 +1,67 @@
 window.addEventListener("DOMContentLoaded", () => {
-  var boton = document.getElementById("boton");
+  var boton = document.getElementById("b1");
+
   boton.addEventListener("click", (e) => {
     e.preventDefault();
-    mostrarDatos();
+    traerDatos();
   });
 });
 
-function mostrarDatos() {
-  fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
-    response.json().then((usuarios) => {
-      console.log(usuarios);
-      //Estructura de la tabla
-      tabla = document.createElement("table");
-      cabecera = document.createElement("thead");
-      tr = document.createElement("tr");
-      document.createElement("th");
-      body = document.createElement("tbody");
+function traerDatos() {
+
+  fetch("https://jsonplaceholder.typicode.com/posts").then((response) =>
+    response.json().then((datos) => { //convierte la respuesta en a JSON
+      console.log(datos);
+      pintarTabla(datos)
     })
   );
+}
+
+function pintarTabla(datos) {
+
+  // Seleccionamos el contenedor donde se insertará la tabla.
+  let divTabla = document.getElementById("tabla-container");
+  divTabla.innerHTML = "";
+
+  // Creamos los elementos de la tabla
+  let tabla = document.createElement("table");
+  let thead = document.createElement("thead");
+  let tbody = document.createElement("tbody");
+
+  // Creamos y añadimos las cabeceras de la tabla.
+  let thTitle = document.createElement("th");
+  thTitle.innerHTML = "Title";
+  thead.appendChild(thTitle);
+
+  let thBody = document.createElement("th");
+  thBody.innerHTML = "Body";
+  thead.appendChild(thBody);
+
+  // Recorremos el array de datos y generamos filas para cada elemento.
+  datos.forEach((dato) => {
+    tbody.appendChild(pintarFila(dato));
+  });
+
+  // Añadimos las partes de la tabla al elemento principal.
+  tabla.appendChild(thead);
+  tabla.appendChild(tbody);
+  
+  // Insertamos la tabla en el contenedor.
+  divTabla.appendChild(tabla);
+}
+
+function pintarFila(usuario) {
+  let tr = document.createElement("tr");
+
+  //celda para el titulo
+  let tdTitle = document.createElement("td");
+  tdTitle.innerHTML = `${usuario.title}`;
+  tr.appendChild(tdTitle);
+
+  //celda para el body
+  let tdBody = document.createElement("td");
+  tdBody.innerHTML = `${usuario.body}`;
+  tr.appendChild(tdBody);
+
+  return tr;
 }
--- 2ºTrimestre/Ayax/Ej2/XMLHttpRequest/js/accion.js
+++ 2ºTrimestre/Ayax/Ej2/XMLHttpRequest/js/accion.js
@@ -1,36 +1,46 @@
 window.addEventListener("DOMContentLoaded", () => {
   var boton = document.getElementById("b1");
-  var datos;
 
   boton.addEventListener("click", (e) => {
     e.preventDefault();
-    datos = traerDatos();
+    traerDatos();
   });
 });
 
 function traerDatos() {
+  //Constante que guarda el objeto XHR
   const peticion = new XMLHttpRequest();
+  // Abrimos una conexión GET con la URL de la API.
   peticion.open("GET", "https://jsonplaceholder.typicode.com/posts");
   //El evento readystatechange comprueba el estado de readyState (Opened, Loading, Done...)
   peticion.addEventListener("readystatechange", () => {
+    // Si el estado no es "completado" (4), salimos de la función.
     if (peticion.readyState !== 4) {
       return;
     }
+    //Si la petición es exitosa
     if (peticion.status >= 200 && peticion.status < 300) {
+      // Parseamos el texto de la respuesta como JSON y lo asignamos a la variable datos.
       datos = JSON.parse(peticion.responseText);
       pintarTabla(datos);
     }
   });
+  //Enviamos la petición
   peticion.send();
 }
 
 function pintarTabla(datos) {
+
+  // Seleccionamos el contenedor donde se insertará la tabla.
   let divTabla = document.getElementById("tabla-container");
   divTabla.innerHTML = "";
+
+  // Creamos los elementos de la tabla
   let tabla = document.createElement("table");
   let thead = document.createElement("thead");
   let tbody = document.createElement("tbody");
 
+  // Creamos y añadimos las cabeceras de la tabla.
   let thTitle = document.createElement("th");
   thTitle.innerHTML = "Title";
   thead.appendChild(thTitle);
@@ -39,12 +49,16 @@ function pintarTabla(datos) {
   thBody.innerHTML = "Body";
   thead.appendChild(thBody);
 
+  // Recorremos el array de datos y generamos filas para cada elemento.
   datos.forEach((dato) => {
     tbody.appendChild(pintarFila(dato));
   });
 
+  // Añadimos las partes de la tabla al elemento principal.
   tabla.appendChild(thead);
   tabla.appendChild(tbody);
+
+  // Insertamos la tabla en el contenedor.
   divTabla.appendChild(tabla);
 }
 
