8f64255a56dd93901fd6eb8594b9bb98dcade984
pvega7721 <pvega7721@ceuandalucia.es>
2025-01-15 17:42:49 +0100

    Agregar imagen en formato WEBP al proyecto

--- 2ºTrimestre/Angular/miPrimerProyecto/src/app/app.component.ts
+++ 2ºTrimestre/Angular/miPrimerProyecto/src/app/app.component.ts
@@ -2,12 +2,31 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  // Define el selector del componente, usado en las plantillas HTML
+  selector: 'app-hola',
+  // Indica que el componente es independiente
   standalone: true,
+  // Importa módulos necesarios para el componente
   imports: [RouterOutlet],
+  // Ruta al archivo de plantilla HTML del componente
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // Ruta al archivo de estilos CSS del componente
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'miPrimerProyecto';
+  title: string = 'Clase de Angular';
+  nombre: string = 'Pablo';
+  edad: number = 18;
+
+  getEdad(): number {
+    return this.edad;
+  }
+
+  mayorEdad(): string {
+    if (this.edad >= 18) {
+      return 'Es mayor de edad';
+    } else {
+      return 'Es menor de edad';
+    }
+  }
 }
