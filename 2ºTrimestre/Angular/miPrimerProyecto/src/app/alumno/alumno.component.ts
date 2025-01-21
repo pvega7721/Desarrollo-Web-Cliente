6d1a3c599d400fd7235a78d19a78d58199860bb9
Pablo Vega <pvega7721@ceuandalucia.es>
2025-01-21 11:07:27 +0100

    Agregar funcionalidades al componente Alumno, incluyendo manejo de DNI, verificación de edad y selección de sexo.

--- 2ºTrimestre/Angular/miPrimerProyecto/src/app/alumno/alumno.component.ts
+++ 2ºTrimestre/Angular/miPrimerProyecto/src/app/alumno/alumno.component.ts
@@ -9,19 +9,42 @@ import { Component } from '@angular/core';
   styleUrl: './alumno.component.css',
 })
 export class AlumnoComponent {
+  //Propiedades del alumno
   nombre: string = 'Pablo';
   apellido: string = 'Vega';
   edad: number = 19;
   dni: string = '12345678a';
   edadx5: number = this.edad * 5;
+  //Mayor o menor de edad con operador ternario
+  mensajeMayorEdad: string = this.edad >= 18 ? 'Mayor de edad' : 'Menor de edad';
 
-  mayorEdad(): string {
-    if (this.edad >= 18) {
-      return 'Es mayor de edad';
-    } else {
-      return 'Es menor de edad';
-    }
+  // Método para actualizar el DNI
+  actualizarDNI(valor: string): void {
+    this.dni = valor;
+  }
+  deshabilitado: boolean = true;
+  desmarcado: boolean = false;
+
+  //Controla que el alumno sea mayor o menor de edad
+  checkMayorMenor(): boolean {
+    return this.edad >=18;
+  }
+  //Al marcar el checkbox, muestra una alerta
+  mensajeMarcado(event: Event): void{
+    alert("Hola");
   }
 
-  desmarcado: boolean = false;
+  check: string = "antes de pulsar";
+
+  cambiarTexto(): void{
+    this.check = "checkbox pulsado";
+  }
+
+  sexoElegido(valor: string): void {
+    if(valor == 'H'){
+      this.check = "Pulsado hombre";
+    } else{
+      this.check = "Pulsado mujer";
+    }
+  }
 }
