89f34067960dc03b8e6d5dcdd52fbe53287c2511
Pablo Vega <pvega7721@ceuandalucia.es>
2025-01-29 12:31:31 +0100

    Agregar componente Articulo con funcionalidad para gestionar artículos y actualizar el formulario de alta de usuario.

--- 2ºTrimestre/Angular/directivas/src/app/app.component.ts
+++ 2ºTrimestre/Angular/directivas/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { CommonModule, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms'; //Para poder usar ngModel
+import { CommonModule, NgIf } from '@angular/common';//Para poder usar ngIf o ngFor
+import { ArticuloComponent } from "./articulo/articulo.component"; 
 
-export interface Persona {
+export interface Persona { //Las interfaces se crean fuera del componente
   nombre: string;
   apellidos: string;
   edad: number;
@@ -12,7 +13,7 @@ export interface Persona {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, ArticuloComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -21,13 +22,30 @@ export class AppComponent {
   nombre: string = '';
   apellido: string = '';
   mostrarMensaje: boolean = false;
-
+  ocultarMensaje: boolean = true;
   enviar() {
+    this.ocultarMensaje = false;
     this.mostrarMensaje = true;
   }
 
-  personas: Persona[] = [
+  personas: Persona[] = [ //Array de objetos de tipo Persona
     { nombre: 'Juan', apellidos: 'Pérez', edad: 30 },
     { nombre: 'Ana', apellidos: 'García', edad: 25 },
+    { nombre: 'Luis', apellidos: 'Martínez', edad: 35 },
+    { nombre: 'María', apellidos: 'López', edad: 28 },
+    { nombre: 'Carlos', apellidos: 'Rodríguez', edad: 40 },
   ];
+
+  boton1: boolean = false;
+  boton2: boolean = false;
+  boton3: boolean = false;
+  mostrarBoton1(){
+    this.boton1 = true;
+  }
+  mostrarBoton2(){
+    this.boton2 = true;
+  }
+  mostrarBoton3(){
+    this.boton3 = true;
+  }
 }
---
+++ 2ºTrimestre/Angular/directivas/src/app/articulo/articulo.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; //Para poder usar ngModel
+import { CommonModule, NgIf } from '@angular/common';//Para poder usar ngIf o ngFor
+import { Articulo } from '../model/articulo.model'; // Importar la interfaz Articulo
+
+@Component({
+  selector: 'app-articulo',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './articulo.component.html',
+  styleUrl: './articulo.component.css'
+})
+export class ArticuloComponent {
+
+  articulos: Articulo[] = [ // Usar la interfaz Articulo para definir el tipo del array
+    { nombre: 'Altavoz', precio: 10, unidades: 16 },
+    { nombre: 'Pantalones', precio: 29, unidades: 22 },
+    { nombre: 'Camiseta', precio: 50, unidades: 14 },
+    { nombre: 'Auriculares', precio: 40, unidades: 4 },
+  ];
+  nombre: string = "";
+  precio: number = 0;
+  unidades: number = 0;
+  comprar(){
+    this.articulos.push({nombre: this.nombre, precio: this.precio, unidades: this.unidades});
+  }
+}
